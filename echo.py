22aad48c62f3ece6f00a12d556f52573ad41ae33
sk25a067 <sk25a067>
2025-02-03 17:05:23 +0900

    [실습1] Echo 프로그램 - 코드 개선하기

--- echo.py
+++ echo.py
@@ -1,6 +1,12 @@
-# 사용자로부터 입력 받기
-user_input = input("문장을 입력해주세요: ")
+while True:
+    # 사용자로부터 입력 받기
+    user_input = input("문장을 입력해주세요: ")
 
-# 입력 받은 문장 그대로 출력
-print("입력하신 문장은:", user_input)
+    # '!quit'을 입력하면 종료
+    if user_input == "!quit":
+        print("프로그램을 종료합니다.")
+        break
+    
+    # 입력 받은 문장 그대로 출력
+    print("입력하신 문장은:", user_input)
 
